e7c2d64dd89076f11c60b5bd82c9d5ac60941eea
linzhiwei <1085101956@qq.com>
2023-06-03 17:24:04 +0800

    图书管理系统

--- app/admin/controller/BooksController.php
+++ app/admin/controller/BooksController.php
@@ -6,9 +6,13 @@ namespace app\admin\controller;
 use app\admin\BaseController;
 use app\admin\extend\helpers\TraitActionHelper;
 use app\admin\validate\ConfigValidate;
+use app\common\helpers\ExportHelper;
+use app\common\helpers\Result;
 use app\common\model\books\Books;
 use app\common\model\Config;
 use app\common\model\demo\Media;
+use app\Request;
+use think\facade\Db;
 
 class BooksController extends BaseController
 {
@@ -16,6 +20,62 @@ class BooksController extends BaseController
 
     protected $model = Books::class;
     protected $validate = ConfigValidate::class;
+    public function indexAction(Request $request)
+    {
+        if ($request->isPost()) {
+            $params = $request->post();
+            if (method_exists($this, 'indexBefore')) {
+                $params = $this->indexBefore($params);
+            }
+            //是否是树形tree，展示所有数据
+            $isTree = $params['isTree'] ?? 0;
 
+            //是否为导出excel，展示所有数据
+            $isExport = $params['isExport'] ?? 0;
+
+            $query = Db::table($this->model::tableName());
+            $query = $this->indexWhere($query, $params);
+
+            //操作查询对象，可以进行语句处理以及数据权限处理
+            $extend = [];
+            $queryResult = $this->indexQuery($query);
+            if(is_array($queryResult)){
+                $query = $queryResult['query'];
+                $extend = $queryResult['extend']??[];
+            }else{
+                $query = $queryResult;
+            }
+
+            //是否分页
+            if (!$isTree && !$isExport) {
+                $pageSize = intval($params['pageSize'] ?? 10);
+                $pageNum = intval($params['pageNum'] ?? 1);
+                $pageNum = $pageNum < 1 ? 1 : $pageNum;
+                $count = $query->count();
+                $query = $query->page($pageNum, $pageSize);
+            }
+            $list = $query->select()->toArray();
+            if ($isTree || $isExport) {
+                $count = count($list);
+            }
+            //结果查询后的处理
+            if (method_exists($this, 'indexAfter')) {
+                $list = $this->indexAfter($list);
+            }
+
+            //导出操作
+            if($isExport){
+                //结果查询后的处理
+                $export_data = $this->exportBefore($list);
+                $excel_path = (new ExportHelper($export_data))->export();
+                return Result::success($excel_path);
+            }else{
+                return Result::success('', $list, ['total' => $count,'extend'=>$extend]);
+            }
+        } else {
+            return $this->indexRender($request);
+        }
+
+    }
 
 }
--- app/admin/controller/demo/MediaController.php
+++ app/admin/controller/demo/MediaController.php
@@ -6,6 +6,7 @@ namespace app\admin\controller\demo;
 
 use app\admin\BaseController;
 use app\admin\extend\helpers\TraitActionHelper;
+use app\admin\validate\RoleValidate;
 use app\common\model\demo\Media;
 
 
@@ -13,5 +14,6 @@ class MediaController extends BaseController
 {
     use TraitActionHelper;
     protected $model = Media::class;
+    protected $validate = RoleValidate::class;
 
 }
